c7568cb7698483f16bbf4c152f06cb7601ea5508
Kasra Hashemi <73937934+zencatalyst@users.noreply.github.com>
2021-11-09 15:09:05 +0330

    Minor string optimizations

--- src/CharMatrix_Exceptions.cpp
+++ src/CharMatrix_Exceptions.cpp
@@ -7,10 +7,10 @@ using namespace peyknowruzi;
 
 const char* Invalid_Fill_Character_Exception::what( ) const throw( )
 {
-	std::string tempMsg( 131, '\0' );
-	tempMsg.clear( );
+	std::string tempMsg;
+	tempMsg.reserve( 131 );
 
-	tempMsg += "Invalid_Fill_Character_Exception: The 'fill character' is not allowed to be one of the following characters: { ";
+	tempMsg = "Invalid_Fill_Character_Exception: The 'fill character' is not allowed to be one of the following characters: { ";
 
 	size_t appendedElementsCount { };
 
@@ -32,12 +32,12 @@ const char* Invalid_Fill_Character_Exception::what( ) const throw( )
 
 const char* Invalid_Y_Axis_Len_Exception::what( ) const throw( )
 {
-	std::string tempMsg( 105, '\0' );
-	tempMsg.clear( );
+	std::string tempMsg;
+    tempMsg.reserve( 105 );
 
-	tempMsg = "Invalid_Y_Axis_Len_Exception: The 'Y-axis length' is not allowed to be greater than " 
-			  + std::to_string( MAX_ALLOWED_Y_AXIS_LEN ) + " and lesser than "
-			  + std::to_string( MIN_ALLOWED_Y_AXIS_LEN ) + ".";
+	tempMsg = "Invalid_Y_Axis_Len_Exception: The 'Y-axis length' is not allowed to be greater than ";
+	tempMsg += std::to_string( MAX_ALLOWED_Y_AXIS_LEN ) + " and lesser than ";
+	tempMsg += std::to_string( MIN_ALLOWED_Y_AXIS_LEN ) + ".";
 
 	char* Invalid_Y_Axis_Len_Exception_Msg = new char[ tempMsg.length( ) + 1 ];
 	std::strcpy( Invalid_Y_Axis_Len_Exception_Msg, tempMsg.c_str( ) );
@@ -47,12 +47,12 @@ const char* Invalid_Y_Axis_Len_Exception::what( ) const throw( )
 
 const char* Invalid_X_Axis_Len_Exception::what( ) const throw( )
 {
-	std::string tempMsg( 106, '\0' );
-	tempMsg.clear( );
+	std::string tempMsg;
+    tempMsg.reserve( 106 );
 
-	tempMsg = "Invalid_X_Axis_Len_Exception: The 'X-axis length' is not allowed to be greater than " 
-			  + std::to_string( MAX_ALLOWED_X_AXIS_LEN ) + " and lesser than "
-			  + std::to_string( MIN_ALLOWED_X_AXIS_LEN ) + ".";
+	tempMsg = "Invalid_X_Axis_Len_Exception: The 'X-axis length' is not allowed to be greater than ";
+	tempMsg	+= std::to_string( MAX_ALLOWED_X_AXIS_LEN ) + " and lesser than ";
+	tempMsg	+= std::to_string( MIN_ALLOWED_X_AXIS_LEN ) + ".";
 
 	char* Invalid_X_Axis_Len_Exception_Msg = new char[ tempMsg.length( ) + 1 ];
 	std::strcpy( Invalid_X_Axis_Len_Exception_Msg, tempMsg.c_str( ) );
