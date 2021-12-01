e4240994060607fb3100a8ef19be4dfaf768b41b
Kasra Hashemi <73937934+zencatalyst@users.noreply.github.com>
2021-12-01 08:23:30 +0330

    Fixed empty buffer causing UB

--- src/Util.cpp
+++ src/Util.cpp
@@ -6,54 +6,69 @@ using namespace peyknowruzi;
 
 inline bool util::tokenize( const std::string_view inputStr, const std::size_t& expectedTokenCount, std::vector< std::string >& foundTokens )
 {
-	std::stringstream ss;
-
-	if ( *( inputStr.data( ) + inputStr.size( ) - 1 ) == '\0' )
+	if ( !inputStr.empty( ) )
 	{
-		ss << inputStr.data( );
+		std::stringstream ss;
+
+		if ( inputStr.back( ) == '\0' )
+		{
+			ss << inputStr.data( );
+		}
+		else
+		{
+			ss << std::string{ inputStr };
+		}
+
+		foundTokens = { std::vector<std::string>( std::istream_iterator<std::string>( ss ), std::istream_iterator<std::string>( ) ) };
+		foundTokens.shrink_to_fit( );
+
+		return ( foundTokens.size( ) == expectedTokenCount ) ? true : false;
 	}
 	else
 	{
-		ss << std::string{ inputStr };
+		return ( expectedTokenCount == 0 ) ? true : false;
 	}
-
-	foundTokens = { std::vector<std::string>( std::istream_iterator<std::string>( ss ), std::istream_iterator<std::string>( ) ) };
-	foundTokens.shrink_to_fit( );
-
-	return ( foundTokens.size( ) == expectedTokenCount ) ? true : false;
 }
 
 int util::isInt( const std::string_view token, bool& is_a_valid_int, const std::pair<int, int>& acceptableRange )
 {
 	int result_int { 0 };
-	std::size_t pos { 0 };
 
-	try
+	if ( !token.empty( ) )
 	{
-		if ( *( token.data( ) + token.size( ) - 1 ) == '\0' )
+		try
 		{
-			result_int = std::stoi( token.data( ), &pos, 10 );
-		}
-		else
-		{
-			result_int = std::stoi( std::string{ token }, &pos, 10 );
-		}
+			std::size_t pos { 0 };
 
-		const auto& [ minAcceptableValue, maxAcceptableValue ] { acceptableRange };
+			if ( token.back( ) == '\0' )
+			{
+				result_int = std::stoi( token.data( ), &pos, 10 );
+			}
+			else
+			{
+				result_int = std::stoi( std::string{ token }, &pos, 10 );
+			}
 
-		if ( pos == token.length( ) && ( result_int <= maxAcceptableValue && result_int >= minAcceptableValue ) )
-		{
-			is_a_valid_int = true;
+			const auto& [ minAcceptableValue, maxAcceptableValue ] { acceptableRange };
+
+			if ( pos == token.length( ) && ( result_int <= maxAcceptableValue && result_int >= minAcceptableValue ) )
+			{
+				is_a_valid_int = true;
+			}
+			else
+			{
+				is_a_valid_int = false;
+				result_int = 0;
+			}
 		}
-		else
-		{
-			is_a_valid_int = false;
-			result_int = 0;
-		}
+		catch ( const std::invalid_argument& ia ) { is_a_valid_int = false; }
+		catch ( const std::out_of_range& oor ) { is_a_valid_int = false; }
+		catch ( const std::exception& e ) { is_a_valid_int = false; }
 	}
-	catch ( const std::invalid_argument& ia ) { is_a_valid_int = false; }
-	catch ( const std::out_of_range& oor ) { is_a_valid_int = false; }
-	catch ( const std::exception& e ) { is_a_valid_int = false; }
+	else
+	{
+		is_a_valid_int = false;
+	}
 
 	return result_int;
 }
@@ -62,7 +77,6 @@ bool util::convert_str_to_valid_ints( const std::string_view inputStr, int* resu
 				   					  const std::vector<int>& specificTokensIndices, const std::pair<int, int>& acceptableRange )
 {
 	std::vector< std::string > foundTokens;
-	foundTokens.reserve( expectedTokenCount );
 
 	bool isAcceptable { util::tokenize( inputStr, expectedTokenCount, foundTokens ) };
 
