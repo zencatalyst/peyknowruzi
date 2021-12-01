9a6e468629a758eae3dd038308f13e86eac54c3f
Kasra Hashemi <73937934+zencatalyst@users.noreply.github.com>
2021-12-01 11:33:37 +0330

    Minor refactoring

--- src/Util.cpp
+++ src/Util.cpp
@@ -6,69 +6,67 @@ using namespace peyknowruzi;
 
 inline bool util::tokenize( const std::string_view inputStr, const std::size_t& expectedTokenCount, std::vector< std::string >& foundTokens )
 {
-	if ( !inputStr.empty( ) )
-	{
-		std::stringstream ss;
-
-		if ( inputStr.back( ) == '\0' )
-		{
-			ss << inputStr.data( );
-		}
-		else
-		{
-			ss << std::string{ inputStr };
-		}
-
-		foundTokens = { std::vector<std::string>( std::istream_iterator<std::string>( ss ), std::istream_iterator<std::string>( ) ) };
-		foundTokens.shrink_to_fit( );
-
-		return ( foundTokens.size( ) == expectedTokenCount ) ? true : false;
-	}
-	else
+	if ( inputStr.empty( ) )
 	{
 		return ( expectedTokenCount == 0 ) ? true : false;
 	}
+
+	std::stringstream ss;
+
+	if ( inputStr.back( ) == '\0' )
+	{
+		ss << inputStr.data( );
+	}
+	else
+	{
+		ss << std::string{ inputStr };
+	}
+
+	foundTokens = { std::vector< std::string >( std::istream_iterator< std::string >( ss ), std::istream_iterator< std::string >( ) ) };
+	foundTokens.shrink_to_fit( );
+
+	return ( foundTokens.size( ) == expectedTokenCount ) ? true : false;
 }
 
 int util::isInt( const std::string_view token, bool& is_a_valid_int, const std::pair<int, int>& acceptableRange )
 {
 	int result_int { 0 };
 
-	if ( !token.empty( ) )
-	{
-		try
-		{
-			std::size_t pos { 0 };
-
-			if ( token.back( ) == '\0' )
-			{
-				result_int = std::stoi( token.data( ), &pos, 10 );
-			}
-			else
-			{
-				result_int = std::stoi( std::string{ token }, &pos, 10 );
-			}
-
-			const auto& [ minAcceptableValue, maxAcceptableValue ] { acceptableRange };
-
-			if ( pos == token.length( ) && ( result_int <= maxAcceptableValue && result_int >= minAcceptableValue ) )
-			{
-				is_a_valid_int = true;
-			}
-			else
-			{
-				is_a_valid_int = false;
-				result_int = 0;
-			}
-		}
-		catch ( const std::invalid_argument& ia ) { is_a_valid_int = false; }
-		catch ( const std::out_of_range& oor ) { is_a_valid_int = false; }
-		catch ( const std::exception& e ) { is_a_valid_int = false; }
-	}
-	else
+	if ( token.empty( ) )
 	{
 		is_a_valid_int = false;
+
+		return result_int;
 	}
+
+	try
+	{
+		std::size_t pos { 0 };
+
+		if ( token.back( ) == '\0' )
+		{
+			result_int = std::stoi( token.data( ), &pos, 10 );
+		}
+		else
+		{
+			result_int = std::stoi( std::string{ token }, &pos, 10 );
+		}
+
+		const auto& [ minAcceptableValue, maxAcceptableValue ] { acceptableRange };
+
+		if ( pos == token.length( ) && ( result_int <= maxAcceptableValue && result_int >= minAcceptableValue ) )
+		{
+			is_a_valid_int = true;
+		}
+		else
+		{
+			is_a_valid_int = false;
+			result_int = 0;
+		}
+	}
+	catch ( const std::invalid_argument& ia ) { is_a_valid_int = false; }
+	catch ( const std::out_of_range& oor ) { is_a_valid_int = false; }
+	catch ( const std::exception& e ) { is_a_valid_int = false; }
 
 	return result_int;
 }
